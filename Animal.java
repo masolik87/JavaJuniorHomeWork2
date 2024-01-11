da7b901e91c52fc670dfcc6e111009dc0568902d
dimon <dimon20.07@mail.ru>
2024-01-11 23:30:52 +0300

    .\Animal.java

---
+++ Animal.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public abstract class Animal {
+    String name;
+    int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
